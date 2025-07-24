d153f1cb4a17e3fae110cacaa147b4480d9b33ff
juango <juango@gmail.com>
2025-07-24 08:36:38 -0500

    ajustes step

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -4,20 +4,15 @@ const Steps = () => {
   return (
     <div className="step_cotizacion">
       <h3>TU CERCA ELÉCTRICA EN SEGUNDOS</h3>
-      <p>
-        En el siguiente formulario, usted podrá obtener el costo aproximado de una cerca eléctrica ganadera adaptada a su necesidad.
-      </p>
-      <h4>Pasos a seguir:</h4>
-      <ol>
-        <li>Ingresa los datos básicos según tu terreno y requerimientos.</li>
-        <li>
-          Recibe una estimación automática. El sistema calculará los materiales recomendados para tu instalación.
-        </li>
-        <li>Explora los productos sugeridos y agrega al carrito.</li>
-        <li>
-          Para validar esta información y confirmar los detalles, por favor contacte a un asesor comercial de <strong>DGASA.com®</strong> a través de nuestros canales autorizados o mediante el botón de contacto disponible en nuestra página web.
-        </li>
-      </ol>
+<p>En el siguiente formulario, usted podrá obtener el costo aproximado de una cerca eléctrica ganadera adaptada a su necesidad.</p>
+<h4>Pasos a seguir:</h4>
+<ol>
+<li>Ingrese los datos básicos según su terreno y requerimientos.</li>
+<li>Reciba una estimación automática. El sistema calculará los materiales recomendados para su instalación.</li>
+<li>Explore los productos sugeridos y agregue al carrito.</li>
+<li>Para validar esta información y confirmar los detalles, por favor contacte a un asesor comercial de DGASA.com® a través de nuestros canales autorizados o mediante el botón de contacto disponible en nuestra página web.</li>
+</ol>
+
       
     </div>
   );
